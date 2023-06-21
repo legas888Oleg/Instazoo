621d8b8ea39f7059c273e8b948290368931fcc6e
Legas <00legas00@gmail.com>
2023-06-21 21:05:13 +0300

    Реализовал модель User

--- src/main/java/ru/legas/instazoo/entity/User.java
+++ src/main/java/ru/legas/instazoo/entity/User.java
@@ -1,4 +1,30 @@
 package ru.legas.instazoo.entity;
 
+import jakarta.persistence.PrePersist;
+import ru.legas.instazoo.entity.enums.ERole;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class User {
+    private Long id;
+    private String name;
+    private String surname;
+    private String middlename;
+    private String email;
+    private String bio;
+    private String username;
+    private String password;
+
+    private Set<ERole> role = new HashSet<>();
+    private List<Post> posts = new ArrayList<>();
+    private LocalDateTime createdDate;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdDate = LocalDateTime.now();
+    }
 }
