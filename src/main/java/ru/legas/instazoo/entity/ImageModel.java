d15c26f616c836662e7478268a77163c96ee12f4
Legas <00legas00@gmail.com>
2023-06-21 21:23:01 +0300

    Реализовал модель ImageModel

--- src/main/java/ru/legas/instazoo/entity/ImageModel.java
+++ src/main/java/ru/legas/instazoo/entity/ImageModel.java
@@ -1,4 +1,9 @@
 package ru.legas.instazoo.entity;
 
 public class ImageModel {
+    private Long id;
+    private String name;
+    private byte[] imageBytes;
+    private Long userID;
+    private Long postID;
 }
