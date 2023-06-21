db6a3378b6dcc4ea8efcea00fe3e3efd51c5a310
Legas <00legas00@gmail.com>
2023-06-21 22:02:13 +0300

    Расстановка аннотиций для ImageModel

--- src/main/java/ru/legas/instazoo/entity/ImageModel.java
+++ src/main/java/ru/legas/instazoo/entity/ImageModel.java
@@ -1,9 +1,26 @@
 package ru.legas.instazoo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
 public class ImageModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Lob
+    @Column(columnDefinition = "LONGBLOB")
     private byte[] imageBytes;
+
+    @JsonIgnore
     private Long userID;
+
+    @JsonIgnore
     private Long postID;
 }
