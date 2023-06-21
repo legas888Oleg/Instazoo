acb77822020b22aa2540d86de716ccc3c6a51765
Legas <00legas00@gmail.com>
2023-06-21 21:05:13 +0300

    Реализовал модель User

---
+++ src/test/java/ru/legas/instazoo/InstazooApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.legas.instazoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstazooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
