57f135cde01077da9ca19a648ff14985b6ceaed9
Legas <00legas00@gmail.com>
2023-06-21 21:52:41 +0300

    Расстановка аннотиций для Post

--- src/main/java/ru/legas/instazoo/entity/Post.java
+++ src/main/java/ru/legas/instazoo/entity/Post.java
@@ -1,6 +1,8 @@
 package ru.legas.instazoo.entity;
 
-import jakarta.persistence.PrePersist;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -8,16 +10,34 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Data
+@Entity
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
     private String title;
+
     private String caption;
+
     private String location;
+
     private Integer likes;
 
+    @Column
+    @ElementCollection(targetClass = String.class)
     private Set<String> likedUsers = new HashSet<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
+
+    @OneToMany(cascade = CascadeType.REFRESH,
+            fetch = FetchType.EAGER, mappedBy = "post", orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
+
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 
     @PrePersist
