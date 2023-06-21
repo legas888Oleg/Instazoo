b4d9807da460beefc6c0eebd1bf4a5bfe361da60
Legas <00legas00@gmail.com>
2023-06-21 21:44:36 +0300

    Расстановка аннотиций для Users

--- src/main/java/ru/legas/instazoo/entity/User.java
+++ src/main/java/ru/legas/instazoo/entity/User.java
@@ -1,27 +1,57 @@
 package ru.legas.instazoo.entity;
 
-import jakarta.persistence.PrePersist;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
 import ru.legas.instazoo.entity.enums.ERole;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
+@Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String surname;
+
+    @Column(nullable = false)
     private String middlename;
+
+    @Column(unique = true)
     private String email;
+
+    @Column(columnDefinition = "text")
     private String bio;
+
+    @Column(unique = true, updatable = true)
     private String username;
+
+    @Column(length = 3000)
     private String password;
 
+    @ElementCollection(targetClass = ERole.class)
+    @CollectionTable(name = "user_role",
+    joinColumns = @JoinColumn("user_id"))
     private Set<ERole> role = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
+
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
     private LocalDateTime createdDate;
+
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
 
     @PrePersist
     protected void onCreate(){
