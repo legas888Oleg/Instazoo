149f7d93952ca838920925b6750c18336d35c70a
Legas <00legas00@gmail.com>
2023-06-22 12:54:31 +0300

    Создал класс LoginRequest. Создал класс SignupRequest. Реализовал данные классы.

---
+++ src/main/java/ru/legas/instazoo/payload/request/LoginRequest.java
@@ -0,0 +1,13 @@
+package ru.legas.instazoo.payload.request;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class LoginRequest {
+    @NotEmpty(message = "Username cannot be empty")
+    private String username;
+    @NotEmpty(message = "Password cannot be empty")
+    private String password;
+}
---
+++ src/main/java/ru/legas/instazoo/payload/request/SignupRequest.java
@@ -0,0 +1,34 @@
+package ru.legas.instazoo.payload.request;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Data
+public class SignupRequest {
+    @Email(message = "It should hava email format")
+    @NotBlank(message = "Your email is required")
+//    @ValidEmail
+    private String email;
+
+    @NotEmpty(message = "Please enter your name")
+    private String name;
+
+    @NotEmpty(message = "Please enter your surname")
+    private String surname;
+
+    @NotEmpty(message = "Please enter your middlename")
+    private String middlename;
+
+    @NotEmpty(message = "Please enter your username")
+    private String username;
+
+    @NotEmpty(message = "Password is required")
+    @Size(min = 6)
+    private String password;
+
+    private String confirmPassword;
+}
