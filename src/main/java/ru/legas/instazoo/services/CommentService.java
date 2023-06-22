90c66da7dc4e7a88e3ec5a1061a2dadd150db1c6
Legas <00legas00@gmail.com>
2023-06-22 18:08:17 +0300

    Создал CommentService.

---
+++ src/main/java/ru/legas/instazoo/services/CommentService.java
@@ -0,0 +1,28 @@
+package ru.legas.instazoo.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.legas.instazoo.repositories.CommentRepository;
+import ru.legas.instazoo.repositories.ImageRepository;
+import ru.legas.instazoo.repositories.PostRepository;
+import ru.legas.instazoo.repositories.UserRepository;
+
+@Service
+public class CommentService {
+    public static final Logger LOG = LoggerFactory.getLogger(CommentService.class);
+
+    private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CommentService(CommentRepository commentRepository,
+                          PostRepository postRepository,
+                          UserRepository userRepository) {
+        this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+    }
+}
