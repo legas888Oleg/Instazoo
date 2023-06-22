ca49ff144b5f5cd9c882af871f98156f8ffa120e
Legas <00legas00@gmail.com>
2023-06-22 18:03:01 +0300

    Рефакторинг

--- src/main/java/ru/legas/instazoo/dto/PostDTO.java
+++ src/main/java/ru/legas/instazoo/dto/PostDTO.java
@@ -2,15 +2,18 @@ package ru.legas.instazoo.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
 import java.util.Set;
 
 @Data
 public class PostDTO {
     private Long id;
+    @NotEmpty
     private String title;
+    @NotEmpty
     private String caption;
     private String location;
     private String username;
     private Integer likes;
-    private Set<String> usersLikes;
+    private Set<String> usersLiked;
 }
