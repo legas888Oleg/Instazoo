ece4da4763cc4d4872e9f55c33935a65fc48dc1f
Legas <00legas00@gmail.com>
2023-06-22 15:14:32 +0300

    Реализовал авторизацию пользователя.

--- src/main/java/ru/legas/instazoo/web/AuthController.java
+++ src/main/java/ru/legas/instazoo/web/AuthController.java
@@ -1,16 +1,21 @@
 package ru.legas.instazoo.web;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.ObjectUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import ru.legas.instazoo.payload.request.LoginRequest;
 import ru.legas.instazoo.payload.request.SignupRequest;
+import ru.legas.instazoo.payload.response.JWTTokenSuccessResponse;
 import ru.legas.instazoo.payload.response.MessageResponse;
 import ru.legas.instazoo.security.JWTTokenProvider;
+import ru.legas.instazoo.security.SecurityConstants;
 import ru.legas.instazoo.services.UserService;
 import ru.legas.instazoo.validations.ResponseErrorValidation;
 
@@ -26,6 +31,24 @@ public class AuthController {
     private AuthenticationManager authenticationManager;
     private ResponseErrorValidation responseErrorValidation;
     private UserService userService;
+
+    @PostMapping("/signin")
+    public ResponseEntity<Object> authenticateUser(
+            @Valid @RequestBody LoginRequest loginRequest, BindingResult bindingResult){
+
+        ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
+        if(!ObjectUtils.isEmpty(errors)) return errors;
+
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginRequest.getUsername(),
+                loginRequest.getPassword()
+        ));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = SecurityConstants.TOKEN_PREFIX + jwtTokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTTokenSuccessResponse(true, jwt));
+    }
 
     @PostMapping("/signup")
     public ResponseEntity<Object> registerUser(
