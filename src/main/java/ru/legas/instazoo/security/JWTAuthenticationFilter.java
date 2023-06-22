bae5ef11e134f9b11142410be1f960bd1157c655
Legas <00legas00@gmail.com>
2023-06-22 11:13:54 +0300

    Переопределил метод doFilterInternal().

--- src/main/java/ru/legas/instazoo/security/JWTAuthenticationFilter.java
+++ src/main/java/ru/legas/instazoo/security/JWTAuthenticationFilter.java
@@ -5,21 +5,48 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+import ru.legas.instazoo.entity.User;
 import ru.legas.instazoo.services.CustomUserDetailsService;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @AllArgsConstructor
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
+    public static final Logger LOG = LoggerFactory.getLogger(JWTTokenProvider.class);
+
     private JWTTokenProvider jwtTokenProvider;
 
     private CustomUserDetailsService customUserDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwt = getJWTFromRequest(request);
 
+            if (StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
+                Long userID = jwtTokenProvider.getUserIdFromToken(jwt);
+                User userDetails = customUserDetailsService.loadUserById(userID);
+
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, Collections.emptyList()
+                );
+
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception ex){
+            LOG.error("Could not set authentication");
+        }
+
+        filterChain.doFilter(request, response);
     }
 
     private String getJWTFromRequest(HttpServletRequest request){
