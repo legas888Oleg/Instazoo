5eb974424cafb428f29ab07a2a1140e5feeaabc4
Legas <00legas00@gmail.com>
2023-06-22 10:18:45 +0300

    Создал JWTTokenProvider и добавил в него Logger.

---
+++ src/main/java/ru/legas/instazoo/security/JWTTokenProvider.java
@@ -0,0 +1,10 @@
+package ru.legas.instazoo.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JWTTokenProvider {
+    public static final Logger LOG = LoggerFactory.getLogger(JWTTokenProvider.class);
+}
