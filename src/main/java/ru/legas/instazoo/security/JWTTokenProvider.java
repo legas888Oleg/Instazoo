e0d00c268658262f2b8a3486f83265fcafa8146a
Legas <00legas00@gmail.com>
2023-06-22 10:34:22 +0300

    Реализация генерации токена

--- src/main/java/ru/legas/instazoo/security/JWTTokenProvider.java
+++ src/main/java/ru/legas/instazoo/security/JWTTokenProvider.java
@@ -1,10 +1,40 @@
 package ru.legas.instazoo.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import ru.legas.instazoo.entity.User;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JWTTokenProvider {
     public static final Logger LOG = LoggerFactory.getLogger(JWTTokenProvider.class);
+
+    public String generateToken(Authentication authentication){
+        User user = (User) authentication.getPrincipal();
+        Date now = new Date(System.currentTimeMillis());
+        Date expiryDate = new Date(now.getTime() + SecurityConstants.EXPIRATION_TIME);
+
+        String userId = Long.toString(user.getId());
+
+        Map<String, Object> clainsMap = new HashMap<>();
+        clainsMap.put("id", userId);
+        clainsMap.put("username", user.getUsername());
+        clainsMap.put("firstname", user.getName());
+        clainsMap.put("lastname", user.getSurname());
+
+        return Jwts.builder()
+                .setSubject(userId)
+                .addClaims(clainsMap)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.SECRET)
+                .compact();
+    }
 }
