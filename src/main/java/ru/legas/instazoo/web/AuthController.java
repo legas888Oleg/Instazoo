811ef5cee75b2d414896149782f1aba8f01a4d44
Legas <00legas00@gmail.com>
2023-06-22 14:53:39 +0300

    Реализовал метод registerUser().

--- src/main/java/ru/legas/instazoo/web/AuthController.java
+++ src/main/java/ru/legas/instazoo/web/AuthController.java
@@ -2,14 +2,21 @@ package ru.legas.instazoo.web;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.util.ObjectUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.legas.instazoo.payload.request.SignupRequest;
 import ru.legas.instazoo.security.JWTTokenProvider;
 import ru.legas.instazoo.services.UserService;
 import ru.legas.instazoo.validations.ResponseErrorValidation;
+
+import javax.validation.Valid;
 
 @CrossOrigin
 @RestController
@@ -21,4 +28,14 @@ public class AuthController {
     private AuthenticationManager authenticationManager;
     private ResponseErrorValidation responseErrorValidation;
     private UserService userService;
+
+    public ResponseEntity<Object> registerUser(
+            @Valid @RequestBody SignupRequest signupRequest, BindingResult bindingResult){
+
+        ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
+        if(!ObjectUtils.isEmpty(errors)) return errors;
+
+        userService.createUser(signupRequest);
+        return ResponseEntity.ok(new MessageResponse("User registered successfully"));
+    }
 }
